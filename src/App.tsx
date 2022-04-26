7cf25c91988bbf88773a8dc6f22bc4dac44be4f4
DAhood1991 <dag07121991@gmail.com>
2022-04-26 10:36:05 +0300

    SECOND COMMIT:CHANGE TITLE IN APP.TSX

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const tasks1 = [
     { id: 3, title: "ReactJS", isDone: false }
 ]
 const tasks2 = [
-    { id: 1, title: "Hello world", isDone: true },
+    { id: 1, title: "HELLO MAZAFAKA", isDone: true },
     { id: 2, title: "I am Happy", isDone: false },
     { id: 3, title: "Yo", isDone: false }
 
